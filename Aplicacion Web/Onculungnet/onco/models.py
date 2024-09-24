a8f8cdce5d748414d740c6397150a3e661f41521
DavidM <davi.merino@duocuc.cl>
2024-09-23 22:55:07 -0300

    Tablas model.py
    se crearon las tablas donde se guardan los datos del formulario para realizar la prediccion

--- Aplicacion Web/Onculungnet/onco/models.py
+++ Aplicacion Web/Onculungnet/onco/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class PatientData(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=[
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ])
+    YELLOW_FINGERS = models.IntegerField()
+    ANXIETY = models.IntegerField()
+    PEER_PRESSURE = models.IntegerField()
+    CHRONIC_DISEASE = models.IntegerField()
+    FATIGUE = models.IntegerField()
+    ALLERGY = models.IntegerField()
+    WHEEZING = models.IntegerField()
+    ALCOHOL_CONSUMING = models.IntegerField()
+    COUGHING = models.IntegerField()
+    SWALLOWING_DIFFICULTY = models.IntegerField()
+    CHEST_PAIN = models.IntegerField()
+    ANXYELFIN = models.IntegerField()
+    probability = models.CharField(null=True, blank=True,max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
